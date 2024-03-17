e2deccad1c5ec2e7d61899834cdd625d27f38802
Krissjop331 <71806054+Krissjop331@users.noreply.github.com>
2024-03-17 10:47:48 +0600

    Добавлена загрузка изображения

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer();
+const uploads = multer();
+const upload = require("../middleware/upload.js");
 
 const UserController = require("../controllers/UserController.js");
-router.get('/', upload.none(), UserController.getUsers);
+const UploadImage = require('../controllers/UploadImage.js');
+
+router.get('/', uploads.none(), UserController.getUsers);
+// router.post('/upload', uploads.single("image"), UploadImage.uploadImage);
+router.post('/', upload.single("image"), UploadImage.uploadImage);
 
 module.exports = router;
